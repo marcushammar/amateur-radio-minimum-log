c3ae8c00069bb1d7c04d3d5bf17be9a3f75c6bfe
Marcus Hammar <marcus@infium.se>
2017-08-22 16:24:22 +0200

    Create empty window

--- src/HamRadioMinimumLog.java
+++ src/HamRadioMinimumLog.java
@@ -1,5 +1,14 @@
-public class HamRadioMinimumLog {
-    public static void main(String[] args) {
+import javax.swing.*;
 
+public class HamRadioMinimumLog extends JFrame {
+
+    private HamRadioMinimumLog(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(450, 350);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new HamRadioMinimumLog();
     }
 }
