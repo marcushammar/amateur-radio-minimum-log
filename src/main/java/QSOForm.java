ca33c637cb36c0af466902fc77d1b9e5d0f89b78
Marcus Hammar <marcus@infium.se>
2017-09-07 14:44:51 +0200

    Add a tool tip to each text field in the QSO dialog

--- src/main/java/QSOForm.java
+++ src/main/java/QSOForm.java
@@ -52,6 +52,22 @@ public class QSOForm extends JPanel{
 
         timeStartTextField.getDocument().addDocumentListener(new TimeTextFieldDocumentListener());
         timeEndTextField.getDocument().addDocumentListener(new TimeTextFieldDocumentListener());
+
+        ToolTipManager.sharedInstance().setDismissDelay(20000);
+
+        callSignTextField.setToolTipText("The call sign of the other person");
+        timeStartTextField.setToolTipText("The start time in the format YYYY-MM-DD HH:MM:SS");
+        timeEndTextField.setToolTipText("The end time in the format YYYY-MM-DD HH:MM:SS");
+        frequencyTextField.setToolTipText("The frequency in MHz in the format NN.NNN (use a dot, not comma)");
+        bandTextField.setToolTipText("Use a small character for the unit (eng. 20m)");
+        modeTextField.setToolTipText("Use large characters for the mode (eng. FT8)");
+        powerTextField.setToolTipText("The power in watts with no decimals (eng. 5)");
+        locationTextField.setToolTipText("Grid square or city of the other person");
+        rstSentTextField.setToolTipText("<html>Signal report in the RST format<br><br>Readability (R)<br>1 - Unreadable<br>2 - Barely readable, occasional words distinguishable<br>3 - Readable with considerable difficulty<br>4 - Readable with practically no difficulty<br>5 - Perfectly readable<br><br>Strength (S)<br>1 - Faint signal, barely perceptible<br>2 - Very weak<br>3 - Weak<br>4 - Fair<br>5 - Fairly good<br>6 - Good<br>7 - Moderately strong<br>8 - Strong<br>9 - Very strong signals<br><br>Tone (T)<br>1 - Sixty cycle a.c or less, very rough and broad<br>2 - Very rough a.c., very harsh and broad<br>3 - Rough a.c. tone, rectified but not filtered<br>4 - Rough note, some trace of filtering<br>5 - Filtered rectified a.c. but strongly ripple-modulated<br>6 - Filtered tone, definite trace of ripple modulation<br>7 - Near pure tone, trace of ripple modulation<br>8 - Near perfect tone, slight trace of modulation<br>9 - Perfect tone, no trace of ripple or modulation of any kind</html>");
+        rstReceivedTextField.setToolTipText("<html>Signal report in the RST format<br><br>Readability (R)<br>1 - Unreadable<br>2 - Barely readable, occasional words distinguishable<br>3 - Readable with considerable difficulty<br>4 - Readable with practically no difficulty<br>5 - Perfectly readable<br><br>Strength (S)<br>1 - Faint signal, barely perceptible<br>2 - Very weak<br>3 - Weak<br>4 - Fair<br>5 - Fairly good<br>6 - Good<br>7 - Moderately strong<br>8 - Strong<br>9 - Very strong signals<br><br>Tone (T)<br>1 - Sixty cycle a.c or less, very rough and broad<br>2 - Very rough a.c., very harsh and broad<br>3 - Rough a.c. tone, rectified but not filtered<br>4 - Rough note, some trace of filtering<br>5 - Filtered rectified a.c. but strongly ripple-modulated<br>6 - Filtered tone, definite trace of ripple modulation<br>7 - Near pure tone, trace of ripple modulation<br>8 - Near perfect tone, slight trace of modulation<br>9 - Perfect tone, no trace of ripple or modulation of any kind</html>");
+        myCallSignTextField.setToolTipText("Your call sign");
+        myLocationTextField.setToolTipText("Your grid square or city");
+        commentsTextField.setToolTipText("Your comments");
     }
 
     public String getCallSign(){
