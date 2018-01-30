870ed4ff5f963387c043625c7350aa3364bcbca5
marcushammar <marcus@infium.se>
2018-01-30 19:49:52 +0100

    Add test suite AllTests

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.SelectClasses;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses(QSOTest.class)
+public class AllTests {
+
+}
