30a28b59ec40a034b3916bbdcfbbeb446fc717aa
marcushammar <marcus@infium.se>
2018-02-10 21:50:03 +0100

    Add sample tests

--- src/test/java/QSOTest.java
+++ src/test/java/QSOTest.java
@@ -3,6 +3,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QSOTest {
+    @Test
+    void getCallSign() {
+        QSO qso = new QSO();
+        qso.setCallSign("AB1CDE");
+        assertEquals("AB1CDE", qso.getCallSign());
+    }
+
+    @Test
+    void getBand() {
+        QSO qso = new QSO();
+        qso.setBand("20m");
+        assertEquals("20m", qso.getBand());
+    }
+
+    @Test
+    void getFrequency() {
+        QSO qso = new QSO();
+        qso.setFrequency("14.074");
+        assertEquals("14.074", qso.getFrequency());
+    }
 
     @Test
     void getComments() {
