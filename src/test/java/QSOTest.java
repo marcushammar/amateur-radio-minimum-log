dbda08408ac7461ea9d0ff916e003e3d0c803e50
marcushammar <marcus@infium.se>
2018-02-12 16:39:12 +0100

    Create test for simple correct QSO

--- src/test/java/QSOTest.java
+++ src/test/java/QSOTest.java
@@ -3,31 +3,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QSOTest {
-    @Test
-    void getCallSign() {
-        QSO qso = new QSO();
-        qso.setCallSign("AB1CDE");
-        assertEquals("AB1CDE", qso.getCallSign());
-    }
 
     @Test
-    void getBand() {
+    void simpleCorrectQSO() {
         QSO qso = new QSO();
-        qso.setBand("20m");
-        assertEquals("20m", qso.getBand());
-    }
+        qso.setField("BAND", "20m");
+        qso.setField("CALL", "FG2HIJ");
+        qso.setField("FREQ", "14.074");
+        qso.setField("GRIDSQUARE", "EF34");
+        qso.setField("MODE", "FT8");
+        qso.setField("MY_GRIDSQUARE", "AB12cd");
+        qso.setField("OPERATOR", "AB1CDE");
+        qso.setField("QSO_DATE", "20180101");
+        qso.setField("QSO_DATE_OFF", "20180101");
+        qso.setField("RST_RCVD", "-12");
+        qso.setField("RST_SENT", "-06");
+        qso.setField("TIME_OFF", "2035");
+        qso.setField("TIME_ON", "2030");
+        qso.setField("TX_PWR", "5");
 
-    @Test
-    void getFrequency() {
-        QSO qso = new QSO();
-        qso.setFrequency("14.074");
-        assertEquals("14.074", qso.getFrequency());
-    }
-
-    @Test
-    void getComments() {
-        QSO qso = new QSO();
-        qso.setComments("Test comment");
-        assertEquals("Test comment", qso.getComments());
+        assertEquals("<BAND:3>20m<CALL:6>FG2HIJ<FREQ:6>14.074<GRIDSQUARE:4>EF34<MODE:3>FT8<MY_GRIDSQUARE:6>AB12cd<OPERATOR:6>AB1CDE<QSO_DATE:8>20180101<QSO_DATE_OFF:8>20180101<RST_RCVD:3>-12<RST_SENT:3>-06<TIME_OFF:4>2035<TIME_ON:4>2030<TX_PWR:1>5<EOR>", qso.getADIF());
     }
 }
