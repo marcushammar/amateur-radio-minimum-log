fecf7d9303b3754c31e3287f9c340878f64e9fe0
marcushammar <marcus@infium.se>
2018-02-20 13:56:14 +0100

    Add validation of QSO fields

--- src/main/java/QSO.java
+++ src/main/java/QSO.java
@@ -16,6 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class QSO {
@@ -60,11 +63,47 @@ public class QSO {
         for (Map.Entry<String, String> pair : this.fields.entrySet()) {
             String value = pair.getValue();
             switch (pair.getKey()) {
+                case "CALL":
+                    validateCall(value);
+                    break;
+                case "QSO_DATE":
+                    validateQsoDate(value);
+                    break;
+                case "TIME_ON":
+                    validateQsoTimeOn(value);
+                    break;
+                case "TIME_OFF":
+                    validateQsoTimeOff(value);
+                    break;
+                case "FREQ":
+                    validateQsoFreq(value);
+                    break;
                 case "BAND":
-                    validateBand(value);
+                    validateQsoBand(value);
                     break;
                 case "MODE":
-                    validateMode(value);
+                    validateQsoMode(value);
+                    break;
+                case "TX_PWR":
+                    validateQsoTxPwr(value);
+                    break;
+                case "GRIDSQUARE":
+                    validateQsoGridsquare(value);
+                    break;
+                case "RST_SENT":
+                    validateQsoRstSent(value);
+                    break;
+                case "RST_RCVD":
+                    validateQsoRstRcvd(value);
+                    break;
+                case "OPERATOR":
+                    validateQsoOperator(value);
+                    break;
+                case "MY_GRIDSQUARE":
+                    validateQsoMyGridsquare(value);
+                    break;
+                case "NOTES":
+                    validateQsoNotes(value);
                     break;
                 default:
                     break;
@@ -72,15 +111,146 @@ public class QSO {
         }
     }
 
-    private void validateBand(String value) {
+    private void validateCall(String value) {
+        if (!isString(value)) {
+            throw new IllegalArgumentException("The CALL field is invalid");
+        }
+    }
+
+    private void validateQsoDate(String value) {
+        if (!isDate(value)) {
+            throw new IllegalArgumentException("The QSO_DATE field is invalid");
+        }
+    }
+
+    private void validateQsoTimeOn(String value) {
+        if (!isTime(value)) {
+            throw new IllegalArgumentException("The TIME_ON field is invalid");
+        }
+    }
+
+    private void validateQsoTimeOff(String value) {
+        if (!isTime(value)) {
+            throw new IllegalArgumentException("The TIME_OFF field is invalid");
+        }
+    }
+
+    private void validateQsoFreq(String value) {
+        if (!isNumber(value)) {
+            throw new IllegalArgumentException("The FREQ field is invalid");
+        }
+    }
+
+    private void validateQsoBand(String value) {
         if (!VALID_BANDS.contains(value)) {
             throw new IllegalArgumentException("The BAND field is invalid");
         }
     }
 
-    private void validateMode(String value) {
+    private void validateQsoMode(String value) {
         if (!VALID_MODES.contains(value)) {
             throw new IllegalArgumentException("The MODE field is invalid");
         }
+    }
+
+    private void validateQsoTxPwr(String value) {
+        if (!isNumber(value)) {
+            throw new IllegalArgumentException("The TX_PWR field is invalid");
+        }
+    }
+
+    private void validateQsoGridsquare(String value) {
+        if (!isGridsquare(value)) {
+            throw new IllegalArgumentException("The GRIDSQUARE field is invalid");
+        }
+    }
+
+    private void validateQsoRstSent(String value) {
+        if (!isString(value)) {
+            throw new IllegalArgumentException("The RST_SENT field is invalid");
+        }
+    }
+
+    private void validateQsoRstRcvd(String value) {
+        if (!isString(value)) {
+            throw new IllegalArgumentException("The RST_RCVD field is invalid");
+        }
+    }
+
+    private void validateQsoOperator(String value) {
+        if (!isString(value)) {
+            throw new IllegalArgumentException("The OPERATOR field is invalid");
+        }
+    }
+
+    private void validateQsoMyGridsquare(String value) {
+        if (!isGridsquare(value)) {
+            throw new IllegalArgumentException("The MY_GRIDSQUARE field is invalid");
+        }
+    }
+
+    private void validateQsoNotes(String value) {
+        if (!isString(value)) {
+            throw new IllegalArgumentException("The NOTES field is invalid");
+        }
+    }
+
+    private static boolean isString(String value) {
+        boolean valid = true;
+        for (int i = 0; i < value.length(); i++) {
+            char c = value.charAt(i);
+            if (c < 32 || c > 126) {
+                valid = false;
+            }
+        }
+        return valid;
+    }
+
+    public static boolean isDate(String date) {
+        try {
+            DateFormat df = new SimpleDateFormat("yyyyMMdd");
+            df.setLenient(false);
+            df.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    public static boolean isTime(String time) {
+        boolean validShort;
+        boolean validLong;
+        try {
+            DateFormat df = new SimpleDateFormat("HHmm");
+            df.setLenient(false);
+            df.parse(time);
+            validShort = true;
+        } catch (ParseException e) {
+            validShort = false;
+        }
+
+        try {
+            DateFormat df = new SimpleDateFormat("HHmmss");
+            df.setLenient(false);
+            df.parse(time);
+            validLong = true;
+        } catch (ParseException e) {
+            validLong = false;
+        }
+
+        return validShort || validLong;
+    }
+
+    public static boolean isNumber(String value) {
+        return value.matches("[0-9]+([.][0-9]+)?");
+    }
+
+    private static boolean isGridsquare(String value) {
+        boolean gridSquareTwoCharacters = value.matches("[A-Z]{2}");
+        boolean gridSquareFourCharacters = value.matches("[A-Z]{2}[0-9]{2}");
+        boolean gridSquareSixCharacters = value.matches("[A-Z]{2}[0-9]{2}[a-z]{2}");
+        boolean gridSquareEightCharacters = value.matches("[A-Z]{2}[0-9]{2}[a-z]{2}[0-9]{2}");
+
+        return gridSquareTwoCharacters || gridSquareFourCharacters || gridSquareSixCharacters || gridSquareEightCharacters;
     }
 }
